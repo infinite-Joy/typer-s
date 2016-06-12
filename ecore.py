d6e5ff6b1e58b9faf5b5bdaebc825f6b71aa3895
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2016-06-12 09:41:17 +0000

    ecore.py pep8

--- ecore.py
+++ ecore.py
@@ -22,81 +22,71 @@ class checkdb(threading.Thread):
         threading.Thread.__init__(self)
         self.start()
     def run(self):
-    	global sdicti
-    	while 1:
-    		score_to_print = ''
-    		time.sleep(20)
-    		fscore = sdicti.scores()
-    		for key in fscore:
-    			if fscore[key] != [0,0]:
-    				score_to_print += str(key) + ' : ' + str(fscore[key]) + '\n'
-    		print('**************************current score*************************' + '\n' + score_to_print)
-
-
-
+        global sdicti
+        while 1:
+            score_to_print = ''
+            time.sleep(20)
+            fscore = sdicti.scores()
+            for key in fscore:
+                if fscore[key] != [0,0]:
+                    score_to_print += str(key) + ' : ' + str(fscore[key]) + '\n'
+            print('**************************current score*************************' + '\n' + score_to_print)
 
 
 class objthread_down(threading.Thread):
-	"""docstring for objthread - handling threads which is creating by key down event from typer-s"""
-	def __init__(self, event_name, event_window, event_time):
-		threading.Thread.__init__(self)
-		self.event_name = event_name
-		self.event_window = event_window
-		self.event_time = event_time
-		self.start()
-	def run(self):
-		global prev_key, dict_counter, counter, dict_time_args
-		if self.event_name != 'Back':
-			prev_key.append(self.event_name)
-			etime = self.event_time.timestamp()
-			counter[0] += 1
-			dict_counter[self.event_name] = counter[0]
-			#print('do', counter, event_name)
-			dict_time_args[counter[0]] = [etime]
-		else:
-			bspacing()
-
+    """docstring for objthread - handling threads which is creating by key down event from typer-s"""
+    def __init__(self, event_name, event_window, event_time):
+        threading.Thread.__init__(self)
+        self.event_name = event_name
+        self.event_window = event_window
+        self.event_time = event_time
+        self.start()
+    def run(self):
+        global prev_key, dict_counter, counter, dict_time_args
+        if self.event_name != 'Back':
+            prev_key.append(self.event_name)
+            etime = self.event_time.timestamp()
+            counter[0] += 1
+            dict_counter[self.event_name] = counter[0]
+            #print('do', counter, event_name)
+            dict_time_args[counter[0]] = [etime]
+        else:
+            bspacing()
 
 
 class objthread_up(threading.Thread):
-	"""docstring for objthread - handling threads which is creating by key up event from typer-s"""
-	def __init__(self, event_name, event_window, event_time):
-		#print(event_name)
-		threading.Thread.__init__(self)
-		self.event_name = event_name
-		self.event_window = event_window
-		self.event_time = event_time
-		self.start()
-	def run(self):
-		global sdicti
-		if self.event_name != 'Back':
-			global avg_time_params, key_dict, dict_counter, dict_time_args, counter
-			etime = self.event_time.timestamp()
-			curr_count = dict_counter[self.event_name]
-			#print('up', curr_count, event_name)
-			dict_time_args[curr_count].append(etime)#for refering previous up time (not using now, for futureq)
+    """docstring for objthread - handling threads which is creating by key up event from typer-s"""
+    def __init__(self, event_name, event_window, event_time):
+        #print(event_name)
+        threading.Thread.__init__(self)
+        self.event_name = event_name
+        self.event_window = event_window
+        self.event_time = event_time
+        self.start()
 
-			curr_hold = etime - dict_time_args[curr_count][0]#curr_up_time - curr_down_time
-			if dict_time_args[curr_count - 1][0]:
-				curr_releasedn = dict_time_args[curr_count][0] - dict_time_args[curr_count - 1][0]#curr_down_time - prev_down_time
-			else:
-				curr_releasedn = 0.0
-			vars()[self.event_name] = key(self.event_name, curr_hold, curr_releasedn)
-			key_dict[curr_count] = vars()[self.event_name]
-			del vars()[self.event_name]
+    def run(self):
+        global sdicti
+        if self.event_name != 'Back':
+            global avg_time_params, key_dict, dict_counter, dict_time_args, counter
+            etime = self.event_time.timestamp()
+            curr_count = dict_counter[self.event_name]
+            #print('up', curr_count, event_name)
+            dict_time_args[curr_count].append(etime)#for refering previous up time (not using now, for futureq)
 
-			if self.event_name == 'Space':
-				sdicti = dicti.checker(key_dict, sdicti)
-				key_dict.clear()
+            curr_hold = etime - dict_time_args[curr_count][0]#curr_up_time - curr_down_time
+            if dict_time_args[curr_count - 1][0]:
+                curr_releasedn = dict_time_args[curr_count][0] - dict_time_args[curr_count - 1][0]#curr_down_time - prev_down_time
+            else:
+                curr_releasedn = 0.0
+            vars()[self.event_name] = key(self.event_name, curr_hold, curr_releasedn)
+            key_dict[curr_count] = vars()[self.event_name]
+            del vars()[self.event_name]
 
-
-
-		
-
+            if self.event_name == 'Space':
+                sdicti = dicti.checker(key_dict, sdicti)
+                key_dict.clear()
 
 
 #Sample codes starts here with test inputs and printing fucntion - can be used for debugging
-
-
 if __name__ == '__main__':
-	print('is this required here')
+    print('is this required here')
